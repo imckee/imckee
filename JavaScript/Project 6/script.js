3a36d73aaa4bc3cf3c021d34f2e58597eb5a10e1
imckee <42479014+imckee@users.noreply.github.com>
2021-03-16 16:29:24 -0500

    Project 6 revision

--- JavaScript/Project 6/script.js
+++ JavaScript/Project 6/script.js
@@ -1,20 +1,57 @@
-var i = 0;
+var colorsArr = ['#4FC3F7', '#29B6F6', '#03A9F4', '#039BE5', '#0288D1', '#0277BD', 
+                '#01579B', '#9575CD', '#7E57C2', '#673AB7', '#5E35B1', '#512DA8', 
+                '#4527A0', '#311B92', '#7986CB', '#5C6BC0', '#3F51B5', '#3949AB', 
+                '#303F9F', '#283593', '#1A237E', '#64B5F6', '#42A5F5', '#2196F3', 
+                '#1E88E5', '#1976D2', '#1565C0', '#0D47A1']
+i = 1;
 
-//I put your myTimer function here so you can call it inside your change function
-function myTimer() {
-  var d = new Date();
-  document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-}
+    
+var showTime = () => {
+    var d = new Date();
+    var second = d.getSeconds();  
+    var minutes = d.getMinutes();
+    var hour = d.getHours();
 
-function change() {
-  var doc = document.getElementById("background");
-  var color = ['#4FC3F7', '#29B6F6', '#03A9F4', '#039BE5', '#0288D1', '#0277BD', '#01579B', '#9575CD', '#7E57C2', '#673AB7', '#5E35B1', '#512DA8', '#4527A0', '#311B92', '#7986CB', '#5C6BC0', '#3F51B5', '#3949AB', '#303F9F', '#283593', '#1A237E', '#64B5F6', '#42A5F5', '#2196F3', '#1E88E5', '#1976D2', '#1565C0', '#0D47A1'];
-  doc.style.backgroundColor = color[i];
-  i = (i + 1) % color.length;
-  myTimer();
-}
+    var hour = d.getHours();
+        if (hour === 0) {
+            hour = 12
+        } else if (hour  > 12) {
+            hour = hour  - 12
+        } else if (hour  <10) {
+            hour = "0" + hour 
+            } else {
+                hour = hour
+            };
 
-setInterval(change, 1000);
+    var minutes = d.getMinutes();
+        if (minutes < 10) {
+            minutes = "0" + minutes
+       } else {
+           console.log(minutes);
+           minutes = minutes
+       };
 
+    var second = d.getSeconds();    
+        if (second< 10) {
+        second= "0" + second
+        } else {
+        second = second
+        };
+    
+    function changeColors() {
+        i+= 1;
+        if (i == colorsArr.length) {
+        i = 0;
+        };
+        document.body.style.backgroundColor = colorsArr[i];
+        };
+    
+    document.getElementById("MyClockDisplay").textContent = 
+    hour + ":" + minutes + ":" + second;
 
-// var myVar = setInterval(myTimer, 1000);
+    setTimeout(showTime, 1000); 
+    changeColors(); 
+
+    }; 
+
+showTime();
