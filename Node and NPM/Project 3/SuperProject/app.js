f63ea622324889e8366db2f3f758bde499aa8fe4
imckee <42479014+imckee@users.noreply.github.com>
2021-04-02 16:53:13 -0500

    Attempt at 5 Node projects

---
+++ Node and NPM/Project 3/SuperProject/app.js
@@ -0,0 +1,19 @@
+var marvel = require('marvel-characters')
+ 
+// // // random character
+console.log(marvel());
+
+console.log(marvel.characters.length);
+
+// var doesItExist = marvel.indexOf('Iron Man');
+// console.log(doesItExist);
+
+// find characters that start with "Man"
+
+let myArray = ['Iron Man'];
+
+console.log(myArray);
+
+console.log(marvel.characters.indexOf('Batman'));
+
+
---
+++ Node and NPM/Project 5/AnimalsProject/zoo.js
@@ -0,0 +1,8 @@
+var animals = require('animals');
+ 
+console.log(animals());
+//=> fox
+ 
+var Log = require('log.pets');
+Log.lion();
+Log.zoo('animals()','animals()','animals()');
