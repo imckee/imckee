3b3ee0e5e37cfcc519c5a4e78d55f30af1222b71
imckee <42479014+imckee@users.noreply.github.com>
2021-04-02 18:17:55 -0500

    Project 3 update

--- Node and NPM/Project 3/SuperProject/app.js
+++ Node and NPM/Project 3/SuperProject/app.js
@@ -4,16 +4,20 @@ var marvel = require('marvel-characters')
 console.log(marvel());
 
 console.log(marvel.characters.length);
+console.log(marvel.characters.filter(name => name.startsWith('Man')));
 
 // var doesItExist = marvel.indexOf('Iron Man');
 // console.log(doesItExist);
 
 // find characters that start with "Man"
+console.log(marvel.characters.filter(name => name.startsWith('Iron Man')));
+console.log(marvel.characters.filter(name => name.startsWith('Batman')));
 
-let myArray = ['Iron Man'];
 
-console.log(myArray);
+// let myArray = ['Iron Man'];
 
-console.log(marvel.characters.indexOf('Batman'));
+// console.log(myArray);
+
+// console.log(marvel.characters.indexOf('Batman'));
 
 
