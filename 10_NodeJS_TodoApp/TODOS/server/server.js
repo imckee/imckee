66d9a5d4d9fb42199f86cdd261ee41e3fbbb9c79
imckee <42479014+imckee@users.noreply.github.com>
2021-04-20 09:21:27 -0500

    TODO revisions

--- 10_NodeJS_TodoApp/TODOS/server/server.js
+++ 10_NodeJS_TodoApp/TODOS/server/server.js
@@ -1,47 +1,57 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser')
 
 const logger = require('morgan');
 app.use(logger('dev'));
 
-// parse application/x-wwww-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
-
+const {toDoArray} = require('./fakeData')
 
 app.get('/', (req, res)=> {
-    res.send('This is the root page.')
+    res.send('Root route');
 })
 
-var toDoArray = [
-    {"id": 1, "description": "Call mom", "isComplete": false},
-    {"id": 2, "description": "Buy groceries", "isComplete": false},
-    {"id": 3, "description": "Go to movies", "isComplete": false}
-]
+// Read - GET
 
-app.get('/todos', (req, res)=> {
-    res.send(toDoArray)
-});
+app.get('/todos', (req, res)=>{
+    res.json(toDoArray);
+})
 
-app.post('/todos', (req, res)=> {
+// Create - POST
+app.post('/todos', (req, res)=>{
     let newTodo = {
-            "id": 4,
-            "description": "Buy more stuff",
-            "isComplete": false
-        }
-        toDoArray.push(newTodo)
-        res.status(200).json(newTodo)
+        id: 4,
+        description: "Buy more stuff!",
+        isComplete: false
+    }
+    toDoArray.push(newTodo);
+    res.json(newTodo);
 })
 
-app.delete('/todos/:id', (req, res)=> {
-    const index = toDoArray.findIndex(4);
-    if (index >= -1) {
-        toDoArray.splice(index, 1);
-    }
-    res.status(200).json(newTodo)
-});
+// Delete - DELETE
+app.delete('/todos/:id', (req, res)=>{
+    let requestedToDoId = parseInt(req.params.id);
+    let requestedToDoIndex = toDoArray.findIndex( todo => {
+        return todo.id === requestedToDoId
+    });
+    console.log(requestedToDoIndex)
+
+    toDoArray.splice(requestedToDoIndex, 1);
+    res.json(toDoArray);
+    // delete todo
+})
+
+// Update - PUT
+app.put('/todos/:id', (req, res)=> {
+    let requestedToDoId = parseInt(req.params.id)
+    let requestedToDo = toDoArray.find(todo => {
+        return todo.id === requestedToDoId;
+    })
+
+    requestedToDo.isComplete = !requestedToDo.isComplete;
+    res.json(requestedToDo);
+    // update something
+})
+
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log(`Listen on port ${port}`))
+app.listen(port, ()=> console.log(`App on port ${port}`));
