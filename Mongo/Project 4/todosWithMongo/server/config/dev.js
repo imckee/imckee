0766493f303e6269d6564fde2195657d599fd06b
imckee <42479014+imckee@users.noreply.github.com>
2021-05-04 13:26:42 -0500

    Project 4 attempt

---
+++ Mongo/Project 4/todosWithMongo/server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb+srv://imckee:nmRBaJ2KP5TdbkOz@bootcamp.yhdcg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
+
+// module.exports = {
+//     mongoURI: "mongodb+srv://imckee:nmRBaJ2KP5TdbkOz@bootcamp.yhdcg.mongodb.net/Classwork?retryWrites=true&w=majority"
+// }
