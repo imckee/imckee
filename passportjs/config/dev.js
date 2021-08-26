df4c679a057d820091bb3f788930493ecacd937e
imckee <42479014+imckee@users.noreply.github.com>
2021-08-26 18:47:25 -0500

    Passport JS project

---
+++ passportjs/config/dev.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     mongoURI: "mongodb+srv://imckee:nmRBaJ2KP5TdbkOz@bootcamp.yhdcg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// }
+
+// module.exports = {
+//     mongoURI: "mongodb+srv://imckee:nmRBaJ2KP5TdbkOz@bootcamp.yhdcg.mongodb.net/Classwork?retryWrites=true&w=majority"
+// }
+
+module.exports = {
+    mongoURI : "mongodb+srv://ghgrave:VNz0ZILiNwcOiERA@demos.ip4u0.mongodb.net/passportim?retryWrites=true&w=majority"
+  }
+  
