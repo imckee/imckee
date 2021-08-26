10421f4b51be398912c57a3bfa477479fbe64132
imckee <42479014+imckee@users.noreply.github.com>
2021-08-26 18:47:44 -0500

    React projects

---
+++ React/Recipe/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import RecipeApp from './RecipeApp';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecipeApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
