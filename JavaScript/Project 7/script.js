e4829d777d28689009abbe0d371c0bf87ad48886
imckee <42479014+imckee@users.noreply.github.com>
2021-08-30 18:19:44 -0500

    Delete JavaScript directory

--- JavaScript/Project 7/script.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Please check the fields and make sure they are not blank.");
-      return false;
-    }
-  }
-  
